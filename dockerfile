471dd6e8b30a359e5a43de8ca20782b700c2429f
Divyansh Sharma DSS <divsh151@gmail.com>
2021-01-27 23:53:52 +0530

    Updates

--- dockerfile
+++ dockerfile
@@ -1,9 +1,16 @@
-# Alpine Linux is much smaller, and thus leads to slimmer images
+# alpine Linux is much smaller, and thus leads to slimmer images
 FROM node:alpine
 
-COPY ./* /usr/node_server/
-
+# change working directory inside docker
+# inside docker it works similar to: mkdir -p /usr/node_server; cd /usr/node_server;
 WORKDIR /usr/node_server
+
+# copy package.json separately to leverage cache in case of code changes w/o any dependency change
+COPY ./package.json ./
 RUN npm install
 
+# copy all data from the folder EXCEPT dockerfile
+COPY ./* ./
+
+# run this command when the container starts
 CMD ["npm", "run", "start"]
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const ip = require('ip');
 const express = require("express");
-
+ 
 const app = express();
 
+port = 8080;
 machineIp = ip.address();
+localAddress = `http://localhost:${port}`;
+machineAddress = `http://${machineIp}:${port}`;
 
 app.get('/', (req, res) => {
     res.send(`Hi There!<br>-From ${machineIp}`)
@@ -11,7 +14,6 @@ app.get('/', (req, res) => {
 
 
 app.listen(8080, ()=> {
-    console.log("Server started..");
-    console.log("Go on to http://localhost:8080");
-    console.log(`..OR try http://${machineIp}:8080`);
+    console.log(`Server started on ${machineAddress}`);
+    console.log(`..OR try ${localAddress}`);
 });
